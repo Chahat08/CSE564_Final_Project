fd765959f048daa1f02e1d0055de9171e57b6f0b
Apoorva Kashi <apoorvakashi@gmail.com>
2024-04-23 13:21:16 -0400

    geomap created

--- project/app.py
+++ project/app.py
@@ -1,3 +1,4 @@
+import csv
 from flask import Flask, render_template, jsonify, request
 from waitress import serve
 import pandas as pd
@@ -5,6 +6,7 @@ import pandas as pd
 app = Flask(__name__)
 
 data = pd.read_csv("static/data/eclipse_removed.csv") 
+geodata = pd.read_csv("static/data/geo_coded_data_clean.csv")
 
 @app.route('/')
 def index():
@@ -12,7 +14,15 @@ def index():
 
 @app.route('/hexbin_plot')
 def hexbin_plot():
-    return "hi"
+    return jsonify({"data": "hexbin data"})
+
+@app.route('/chloropleth')
+def chloropleth():
+    with open('../data/geo_coded_data_clean.csv', 'r') as csvfile:
+        reader = csv.DictReader(csvfile)
+        data = list(reader)
+    return jsonify(data)
 
 if __name__ == '__main__':
-    serve(app, host='0.0.0.0', port=8080)
+    # serve(app, host='0.0.0.0', port=8080)
+    app.run(debug=True)
